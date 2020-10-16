ec5844b758a76382ac335a7aa20334de5dc1a54b
Loucif RAKEM <loucif.rakem@edu.univ-fcomte.fr>
2020-10-16 17:51:56 +0200

    Updates

---
+++ precache-manifest.6996ce4cfb388a966af2f92a579b4022.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690ff0898c0b45b5ebdb8fef76354456",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a658c11a825f120944",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "84fc1b262b2a1b080576",
+    "url": "/static/css/main.73996b2b.chunk.css"
+  },
+  {
+    "revision": "79a658c11a825f120944",
+    "url": "/static/js/2.3cbdca7c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3cbdca7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fc1b262b2a1b080576",
+    "url": "/static/js/main.99802eda.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
